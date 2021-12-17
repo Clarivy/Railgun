9616517393413a753da3210f8d80f86c42764f8b
Clarivy <zqc.sc.hn@gmail.com>
2021-12-17 12:23:42 +0800

    Fix conflict

--- circle.py
+++ circle.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-import cv2
-class Circles:
-    def __init__(self, debug = False):
-        self.debug = debug
-        
-    def center(self,img,zoom=1,thres = 20):
-        height, width = img.shape[:2]
-        output = cv2.resize(img, (0, 0), fx=zoom, fy=zoom)
-        image = cv2.resize(img, (0, 0), fx=zoom, fy=zoom)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 1)
-        if circles is not None:
-            if circles.shape[1] > thres:
-                circles = np.median(circles,axis=0)
-                circles = np.round(circles[0, :]).astype("int")
-                x, y, r = circles
-                if self.debug:
-                    cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-                    cv2.putText(output, str((-(width//2 - x // zoom), height//2 - y // zoom)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX,
-                                0.75, (0, 0, 225), 2)
-                    cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-                    cv2.imshow("output", output)
-                return ((-(width // 2 - x // zoom), height // 2 - y // zoom))
-            else:
-                return None
-        else:
-            return None
-=======
 import numpy as np
 import cv2
 class Circles:
@@ -55,4 +25,3 @@ class Circles:
                 return None
         else:
             return None
->>>>>>> ae4ccd4f5ea8ee1012c5d282ce0f126478fb8e04
